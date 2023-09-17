2365e03fa5d4d89c2f8edb541a7b80c622210849
Vu20020814 <nguyenvanvu20020814@gmail.com>
2023-09-17 16:59:58 +0700

    promise chain basic

--- Demo/promise.js
+++ Demo/promise.js
@@ -1,3 +1,7 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+new Promise((resolve) => resolve("get data "))
+  .then(console.log("1"))
+  .then(console.log("2"))
+  .then(console.log("3"));
